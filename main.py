6887d521e4397add475e71235b9f96a1932beb85
examplesOfWorks <webDissss@gmail.com>
2026-08-07 19:26:10 +0300

    post-запрос: добавление заметки

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -17,14 +18,24 @@ notes = [
     }
 ]
 
+class NoteCreate(BaseModel):
+    title: str = Field(
+        min_length=1,
+        max_length=100
+    )
+    text: str = Field(
+        min_length=1,
+        max_length=5000
+    )
+
 @app.get("/notes")
 def read_notes(search: str | None = None, limit: int | None = None):
     result = notes
     if search:
         result = [note for note in notes if search.lower() in note["title"].lower()]
-    if limit and limit >= 1:
+    if limit is not None:
         result = result[:limit]
-    elif limit and limit < 1:
+    if limit is not None and limit < 1:
         raise HTTPException(status_code=400, detail="Limit must be positive")
     return result
 
@@ -35,7 +46,12 @@ def read_note(note_id: int):
             return note
     raise HTTPException(status_code=404, detail="Note not found")
         
-
+@app.post("/notes")
+def create_note(note: NoteCreate):
+    new_note = note.model_dump()
+    new_note["id"] = len(notes) + 1
+    notes.append(new_note)
+    return new_note
 
 
 if __name__ == "__main__":
